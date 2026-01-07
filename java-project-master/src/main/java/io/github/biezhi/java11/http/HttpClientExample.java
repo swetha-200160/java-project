612c6962d7127acaec89ab2f649dd91591ac15a8
Swetha Suresh <swethasuresh@canvendor.com>
2026-01-07 16:19:35 +0530

    adding files

--- java-project-master/src/main/java/io/github/biezhi/java11/http/HttpClientExample.java
+++ java-project-master/src/main/java/io/github/biezhi/java11/http/HttpClientExample.java
@@ -1,16 +1,14 @@
-package io.github.biezhi.java11;
-
-import java.io.FileWriter;
-import java.time.LocalDateTime;
+package io.github.biezhi.java11.http;
 
 public class HttpClientExample {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Java app started...");
+        System.out.println("Application started...");
 
+        // Keep application running
         while (true) {
             Thread.sleep(10000);
-            System.out.println("App running...");
+            System.out.println("Application is running...");
         }
     }
 }
