512c6c8b57a96bbe6d13b7526c74735790852046
Swetha Suresh <swethasuresh@canvendor.com>
2026-01-07 18:05:50 +0530

    adding jenkins file

--- java-project-master/src/main/java/io/github/biezhi/java11/Application.java
+++ java-project-master/src/main/java/io/github/biezhi/java11/Application.java
@@ -4,9 +4,9 @@ public class Application {
     public static void main(String[] args) throws Exception {
         System.out.println("Java app started");
 
-        // keep container alive
         while (true) {
             Thread.sleep(60000);
         }
     }
 }
+
