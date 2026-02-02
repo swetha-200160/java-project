9c98b1c562e21553a2a036a742371eae2e87976a
Swetha Suresh <swethasuresh@canvendor.com>
2026-02-02 12:57:54 +0530

    Adding file

--- java-project-master/src/main/java/HelloServer.java
+++ java-project-master/src/main/java/HelloServer.java
@@ -1,16 +1,29 @@
+package com.example;
+
 import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 public class HelloServer {
+
     public static void main(String[] args) throws Exception {
 
-        int port = 9090; // default
-
+        int port = 9090;
         if (System.getenv("APP_PORT") != null) {
             port = Integer.parseInt(System.getenv("APP_PORT"));
         }
 
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        server.createContext("/", exchange -> {
+            String response = "✅ Server is running on port " + port;
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.setExecutor(null);
         server.start();
 
         System.out.println("Server started on port " + port);
