987961bb2edd7c8be7bd86f59e8892a8b7a46fc5
Swetha Suresh <swethasuresh@canvendor.com>
2026-02-02 12:35:42 +0530

    adding jenkins file

--- java-project-master/src/main/java/HelloServer.java
+++ java-project-master/src/main/java/HelloServer.java
@@ -1,24 +1,19 @@
-package com.example;
-
 import com.sun.net.httpserver.HttpServer;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
 public class HelloServer {
-
     public static void main(String[] args) throws Exception {
-        int port = 9090;
+
+        int port = 9090; // default
+
+        if (System.getenv("APP_PORT") != null) {
+            port = Integer.parseInt(System.getenv("APP_PORT"));
+        }
 
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-        server.createContext("/", exchange -> {
-            String response = "Hello from JAR running on a server!";
-            exchange.sendResponseHeaders(200, response.getBytes().length);
-            try (OutputStream os = exchange.getResponseBody()) {
-                os.write(response.getBytes());
-            }
-        });
-
         server.start();
+
         System.out.println("Server started on port " + port);
     }
 }
+
