021daa5d999605b75394e5b570afbf7337c2febc
Swetha Suresh <swethasuresh@canvendor.com>
2026-02-02 10:42:27 +0530

    adding java file

--- java-project-master/src/main/java/io/github/biezhi/java11/Application.java
+++ java-project-master/src/main/java/io/github/biezhi/java11/Application.java
@@ -1,12 +1,24 @@
-package io.github.biezhi.java11;
+package com.example;
 
-public class Application {
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class HelloServer {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Java app started");
+        int port = 8080;
 
-        while (true) {
-            Thread.sleep(60000);
-        }
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", exchange -> {
+            String response = "Hello from JAR running on a server!";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.getBytes());
+            }
+        });
+
+        server.start();
+        System.out.println("Server started on port " + port);
     }
 }
-
