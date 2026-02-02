3f7a7bb67d4ff74040b9cab61ffab2877aa6fc85
Swetha Suresh <swethasuresh@canvendor.com>
2026-02-02 11:06:59 +0530

    adding files

--- java-project-master/src/main/java/HelloServer.java
+++ java-project-master/src/main/java/HelloServer.java
@@ -7,7 +7,7 @@ import java.net.InetSocketAddress;
 public class HelloServer {
 
     public static void main(String[] args) throws Exception {
-        int port = 8080;
+        int port = 9090;
 
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         server.createContext("/", exchange -> {
