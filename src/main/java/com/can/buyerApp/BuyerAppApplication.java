5ec0b0d781b221a1f23526db5f35af096e10a90b
Swetha Suresh <swethasuresh@canvendor.com>
2026-02-17 10:41:16 +0530

    adding jenkins file

--- src/main/java/com/can/buyerApp/BuyerAppApplication.java
+++ src/main/java/com/can/buyerApp/BuyerAppApplication.java
@@ -1,12 +1,12 @@
-package com.can.buyerApp;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BuyerAppApplication {
-
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(BuyerAppApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
+
