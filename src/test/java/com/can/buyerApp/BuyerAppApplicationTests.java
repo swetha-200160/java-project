ccbe43060f661806cfe71c30e96c3f06cd1f6543
Swetha Suresh <swethasuresh@canvendor.com>
2026-02-18 12:09:37 +0530

    adding files

--- src/test/java/com/can/buyerApp/BuyerAppApplicationTests.java
+++ src/test/java/com/can/buyerApp/BuyerAppApplicationTests.java
@@ -1,16 +1,13 @@
 package com.can.buyerApp;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.can.buyerApp.repository")
-@EntityScan(basePackages = "com.can.buyerApp.entity")
-public class BuyerAppApplication {
+@SpringBootTest
+class BuyerAppApplicationTests {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BuyerAppApplication.class, args);
+    @Test
+    void contextLoads() {
     }
 }
+
