f81f2c6d59f468e9779f421bc91fa1c97998cdfd
Swetha Suresh <swethasuresh@canvendor.com>
2026-04-07 12:09:22 +0530

    adding files

---
+++ src/main/java/com/can/buyerApp/service/InsuranceServiceTest.java
@@ -0,0 +1,13 @@
+package com.can.buyerApp.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class InsuranceServiceTest {
+
+    @Test
+    void testSample() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+}
