9f203c293ceb7411839e2ded3937477d9fdcde2d
Swetha Suresh <swethasuresh@canvendor.com>
2026-04-10 09:28:28 +0530

    adding files

--- src/test/java/com/can/buyerApp/SampleTest.java
+++ src/test/java/com/can/buyerApp/SampleTest.java
@@ -1,7 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SampleTest {
-    
-    @org.junit.jupiter.api.Test
-    void test1() {
-        System.out.println("Test 1 running");
+
+    @Test
+    void testExample() {
+        int result = 2 + 3;
+        assertEquals(5, result);
     }
 }
